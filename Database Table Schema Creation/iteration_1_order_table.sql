ab642f35de2a2902a6fd5ff4b1508123ca67bf30
ChristianT-Stu <69771935+ChristianT-Stu@users.noreply.github.com>
2023-04-14 12:23:30 -0400

    First Iteration in Database Scheme for Pizza-DB-Project

---
+++ Database Table Schema Creation/iteration_1_order_table.sql
@@ -0,0 +1,28 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE `order` (
+    `row_id` int  NOT NULL ,
+    `order_id` varchar(10)  NOT NULL ,
+    `created_at` datetime  NOT NULL ,
+    `item_name` varchar(50)  NOT NULL ,
+    `item_cat` varchar(50)  NOT NULL ,
+    `item_size` varchar(20)  NOT NULL ,
+    `item_price` decimal(5,2)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    `cust_firstname` varchar(50)  NOT NULL ,
+    `cust_lastname` varchar(50)  NOT NULL ,
+    `delivery` boolean  NOT NULL ,
+    `delivery_address1` varchar(200)  NOT NULL ,
+    `delivery_address2` varchar(200)  NOT NULL ,
+    `delivery_city` varchar(50)  NOT NULL ,
+    `delivery_zipcode` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
