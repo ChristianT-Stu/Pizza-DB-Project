3aed71dc45e721c74c255d4b545532dfd280c763
ChristianT-Stu <69771935+ChristianT-Stu@users.noreply.github.com>
2023-04-15 13:01:14 -0400

    Create iteration_2_cust_add.sql

---
+++ Database Table Schema Creation/iteration_2_cust_add.sql
@@ -0,0 +1,47 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE `order` (
+    `row_id` int  NOT NULL ,
+    `order_id` varchar(10)  NOT NULL ,
+    `created_at` datetime  NOT NULL ,
+    `item_name` varchar(50)  NOT NULL ,
+    `item_cat` varchar(50)  NOT NULL ,
+    `item_size` varchar(20)  NOT NULL ,
+    `item_price` decimal(5,2)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    `cust_id` int  NOT NULL ,
+    `delivery` boolean  NOT NULL ,
+    `add_id` int  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+CREATE TABLE `customers` (
+    `cust_id` int  NOT NULL ,
+    `cust_firstname` varchar(50)  NOT NULL ,
+    `cust_lastname` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `cust_id`
+    )
+);
+
+CREATE TABLE `address` (
+    `add_id` int  NOT NULL ,
+    `delivery_address1` varchar(200)  NOT NULL ,
+    `delivery_address2` varchar(200)  NULL ,
+    `delivery_city` varchar(50)  NOT NULL ,
+    `delivery_zipcode` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `add_id`
+    )
+);
+
+ALTER TABLE `customers` ADD CONSTRAINT `fk_customers_cust_id` FOREIGN KEY(`cust_id`)
+REFERENCES `order` (`cust_id`);
+
+ALTER TABLE `address` ADD CONSTRAINT `fk_address_add_id` FOREIGN KEY(`add_id`)
+REFERENCES `order` (`add_id`);
+
